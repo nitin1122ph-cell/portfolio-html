0a5d66ea7e74094fa5209538e140c0d540b135bb
Nitin Phadtare <nitin1122ph@gmail.com>
2026-03-10 17:47:55 +0530

    Updated portfolio UI with animations and WordPress project links

--- script.js
+++ script.js
@@ -5,3 +5,42 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       .scrollIntoView({ behavior: 'smooth' });
   });
 });
+
+const text = "Wordpress & Frontend Developer";
+let index = 0;
+
+function typingEffect() {
+
+if(index < text.length){
+
+document.getElementById("typing").innerHTML += text.charAt(index);
+
+index++;
+
+setTimeout(typingEffect,100);
+
+}
+
+}
+
+typingEffect();
+
+
+
+const elements = document.querySelectorAll(".fade-in");
+
+window.addEventListener("scroll", ()=>{
+
+elements.forEach(el=>{
+
+const position = el.getBoundingClientRect().top;
+
+if(position < window.innerHeight - 100){
+
+el.classList.add("show");
+
+}
+
+});
+
+});
